457f0f15750041d3943748939a1ea78195c8c64e
ngochau202501 <93257827+ngochau202501@users.noreply.github.com>
2021-11-01 10:37:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ const CONFIG = {
     desc: 'Chị có muốn chơi game với em không? ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'tại sao chị lại muốn chơi chơi với em',
+    question: 'Chị thấy em là người như thế nào',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'tại em em dễ thương 10đ',
-    mess: 'Em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
+    reply: ' em dễ thương 10đ',
+    mess: 'Em biết mà 🥰.Cảm ơn chị nhiều nhiều 😘😘',
     messDesc: 'Tối nay 10h, mình leo rank nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/ngochaucutee123/deptrai10d' //link mess của các bạn. VD: https://m.me/nam.nodemy
